f6d72608e54cb67e9689f3c924c7ad42140f2b81
thiago <thiagoedusan5.11@outlook.com>
2023-03-29 16:30:59 -0300

    ...

--- src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
+++ src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
@@ -6,7 +6,12 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Principal {
+
+  /**
+   * Método principal.
+   */
   public static void main(String[] args) {
+    // ESCREVA SEU CÓDIGO AQUI
     Locale.setDefault(Locale.US);
 
     Scanner scanner = new Scanner(System.in);
